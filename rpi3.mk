3edc19a350e48176a44dc98d81b27568762efb82
Alex Tu <alex.tu@canonical.com>
2022-06-09 00:07:44 +0800

    hellotoast HIDL service

--- rpi3.mk
+++ rpi3.mk
@@ -55,6 +55,8 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-impl.rpi3 \
     android.hardware.audio@2.0-impl \
     android.hardware.audio.effect@2.0-impl \
+    android.hardware.helloalex@1.0-service \
+    android.hardware.hellotoast@1.0-service \
     android.hardware.keymaster@3.0-impl \
     android.hardware.memtrack@1.0-service \
     android.hardware.memtrack@1.0-impl \
